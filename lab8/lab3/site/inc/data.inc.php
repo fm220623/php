adada92be3d3a66dece69c7ac56148df33b8540b
fm220623 <161931314+fm220623@users.noreply.github.com>
2024-12-21 11:33:42 +0300

    CORRECT

--- lab8/lab3/site/inc/data.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$menuItems = [
-    ['link' => 'Домой', 'href' => 'index.php'],
-    ['link' => 'О нас', 'href' => 'about.php'],
-    ['link' => 'Контакты', 'href' => 'contact.php'],
-    ['link' => 'Таблица умножения', 'href' => 'table.php'],
-    ['link' => 'Калькулятор', 'href' => 'calc.php']
-];
-?>
--- lab8/lab4/site/inc/data.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$leftMenu = [
-    ['href' => 'index.php?id=about', 'link' => 'О нас'],
-    ['href' => 'index.php?id=contact', 'link' => 'Контакты'],
-    ['href' => 'index.php?id=table', 'link' => 'Таблица умножения'],
-    ['href' => 'index.php?id=calc', 'link' => 'Калькулятор'],
-];
-?>
